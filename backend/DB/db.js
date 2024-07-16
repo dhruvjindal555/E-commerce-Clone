6a3fd7a9fa1462965cef4beff954ac9c1e95d656
Dhruv jindal <124202030+dhruvjindal555@users.noreply.github.com>
2024-07-16 20:44:58 +0530

    Initial Commit

---
+++ backend/DB/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+const url = process.env.MONGO_URL
+async function connectToDatabse() {
+    try {
+        await mongoose.connect(url).then(() => {
+            console.log("Successfully Connected to Database")
+        })
+    } catch (e) {
+        console.log("Error connecting to Database")
+        console.log(e.message);
+    }
+}
+
+module.exports = connectToDatabse
+
---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 8888
+var cors = require('cors')
+const connectToDatabse = require('../backend/DB/db')
+connectToDatabse()
+
+app.use(express.json())
+app.use(cors())
+app.use('/auth', require('./Routes/auth'))
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
